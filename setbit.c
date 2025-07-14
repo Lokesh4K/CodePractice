8b02b71976c13469e5096d16ea1dd6afad455c78
LokeshKumar <81944189+Lokesh4K@users.noreply.github.com>
2025-07-14 07:35:40 +0000

    applied Brian Kernighan's method

--- setbit.c
+++ setbit.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdarg.h>
-
+#define METHOD_2 
+/*
+METHOD_2 is Brian Kernighan's Algorithm
+Which is n = n & (n-1); clears the Set LSBit
+If we repeat this operation till it clears all the LSBit, which gives the count of set bit in number 
+*/
 #define NO_OF_BITS_IN_BYTE  8
+
 int main() {
+#ifdef METHOD_1    
     int val = 0x5555;
     int noOfBitSet = 0;
     
@@ -19,5 +26,25 @@ int main() {
         break;
     }
     printf("bit set are %d/n/r", noOfBitSet);
-    return 0;
+#endif ENDIF
+
+#ifdef METHOD_2
+int num = 0;
+scanf("%d", &num);
+printf("Enterd num is %b/n/r", num);
+int count = 0;
+while(num)
+{
+    num = num & (num-1);
+    count++;
 }
+printf("bit set are %d/n/r", count);
+#endif
+return 0;
+}
+
+/*
+Result:
+111
+Enterd num is 1101111/n/rbit set are 6/n/r[1] + Done
+*/
