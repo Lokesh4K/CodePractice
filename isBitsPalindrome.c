78feef0fbe16de2611a0dbbde10c8bf3c756cbb1
LokeshKumar <81944189+Lokesh4K@users.noreply.github.com>
2025-07-18 10:30:14 +0000

    added another method of checking

--- isBitsPalindrome.c
+++ isBitsPalindrome.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+//At the end is another program for this
+
 unsigned char isNumBitPalindrome(unsigned char num)
 {
     for (int i = 0; i < 4; i++)
@@ -50,3 +52,35 @@ Binary of num 0b10000
 
 No[?2004h
 */
+
+
+#if 0
+
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t isBinaryPalindrome(uint32_t num)
+{
+    for(int i = 0; i<32; i++)
+    {
+        if (((num >> i) & 0x01) != ((num >> 32-i-1) & 0x01))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    printf("Hello World\n\r");
+    uint32_t val = 0x000FF00F;
+    
+    if (isBinaryPalindrome(val))
+    printf("Yess");
+    else
+    printf("ohh no");
+    return 0;
+}
+
+#endif
